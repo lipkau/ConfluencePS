66a77f29f2e4f38dcc14b4919db5a8f8632399f0
Oliver Lipkau <oliver@lipkau.net>
2018-12-15 13:34:49 +0100

    Updated documentation and tests on Get-Space

---
+++ Tests/Functions/Get-Space.Unit.Tests.ps1
@@ -0,0 +1,237 @@
+#requires -modules BuildHelpers
+#requires -modules @{ ModuleName = "Pester"; ModuleVersion = "4.4.2" }
+
+Describe "Get-Space" -Tag Unit {
+
+    BeforeAll {
+        Remove-Item -Path Env:\BH*
+        $projectRoot = (Resolve-Path "$PSScriptRoot/../..").Path
+        if ($projectRoot -like "*Release") {
+            $projectRoot = (Resolve-Path "$projectRoot/..").Path
+        }
+
+        Import-Module BuildHelpers
+        Set-BuildEnvironment -BuildOutput '$ProjectPath/Release' -Path $projectRoot -ErrorAction SilentlyContinue
+
+        $env:BHManifestToTest = $env:BHPSModuleManifest
+        $script:isBuild = $PSScriptRoot -like "$env:BHBuildOutput*"
+        if ($script:isBuild) {
+            $Pattern = [regex]::Escape($env:BHProjectPath)
+
+            $env:BHBuildModuleManifest = $env:BHPSModuleManifest -replace $Pattern, $env:BHBuildOutput
+            $env:BHManifestToTest = $env:BHBuildModuleManifest
+        }
+
+        Import-Module "$env:BHProjectPath/Tools/BuildTools.psm1"
+
+        Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
+        Import-Module $env:BHManifestToTest
+    }
+    AfterAll {
+        Remove-Module $env:BHProjectName -ErrorAction SilentlyContinue
+        Remove-Module BuildHelpers -ErrorAction SilentlyContinue
+        Remove-Item -Path Env:\BH*
+    }
+
+    #region Mocking
+    Mock Write-DebugMessage -ModuleName $env:BHProjectName {}
+    Mock Write-Verbose -ModuleName $env:BHProjectName {}
+
+    Mock Invoke-Method -ModuleName $env:BHProjectName -ParameterFilter {
+        $Uri -like "/rest/api/space*" -and
+        $Method -eq "GET"
+    } {}
+    Mock Invoke-Method -ModuleName $env:BHProjectName { throw "Invalid call to Invoke-Method" }
+    #endregion Mocking
+
+    Context "Sanity checking" {
+
+        $command = Get-Command -Name Get-ConfluenceSpace
+
+        It "has a [AtlassianPS.ConfluencePS.Space[]] -Space parameter" {
+            $command.Parameters.ContainsKey("Space")
+            $command.Parameters["Space"].ParameterType | Should -Be "AtlassianPS.ConfluencePS.Space[]"
+        }
+
+        It "has a [UInt32] -PageSize parameter" {
+            $command.Parameters.ContainsKey("PageSize")
+            $command.Parameters["PageSize"].ParameterType | Should -Be "UInt32"
+        }
+
+        It "has a [String] -ServerName parameter" {
+            $command.Parameters.ContainsKey("ServerName")
+            $command.Parameters["ServerName"].ParameterType | Should -Be "String"
+        }
+
+        It "has an ArgumentCompleter for -ServerName" {
+            $command.Parameters["ServerName"].Attributes |
+                Where-Object {$_ -is [ArgumentCompleter]} |
+                Should -Not -BeNullOrEmpty
+        }
+
+        It "has a [PSCredential] -Credential parameter" {
+            $command.Parameters.ContainsKey('Credential')
+            $command.Parameters["Credential"].ParameterType | Should -Be "PSCredential"
+        }
+    }
+
+    Context "Behavior checking" {
+
+        It "fetches all Spaces the user has permissions to see" {
+            Get-ConfluenceSpace
+
+            $assertMockCalledSplat = @{
+                CommandName     = "Invoke-Method"
+                ModuleName      = $env:BHProjectName
+                ParameterFilter = {
+                    $Uri -eq "/rest/api/space"
+                }
+                Exactly         = $true
+                Times           = 1
+                Scope           = 'It'
+            }
+            Assert-MockCalled @assertMockCalledSplat
+        }
+
+        It "fetches the requested Spaces" {
+            Get-ConfluenceSpace -Space "Foo"
+            Get-ConfluenceSpace -Space "Bar", "Baz"
+
+            $assertMockCalledSplat = @{
+                CommandName     = "Invoke-Method"
+                ModuleName      = $env:BHProjectName
+                ParameterFilter = {
+                    $Uri -match "\/rest\/api\/space\/[Foo|Bar|Baz]"
+                }
+                Exactly         = $true
+                Times           = 3
+                Scope           = 'It'
+            }
+            Assert-MockCalled @assertMockCalledSplat
+        }
+
+        It "returns [AtlassianPS.ConfluencePS.Space] objects" {
+            Get-ConfluenceSpace
+            Get-ConfluenceSpace -Space "Foo", "Bar"
+
+            $assertMockCalledSplat = @{
+                CommandName     = "Invoke-Method"
+                ModuleName      = $env:BHProjectName
+                ParameterFilter = {
+                    $Uri -like "/rest/api/space*" -and
+                    $OutputType -eq [AtlassianPS.ConfluencePS.Space]
+                }
+                Exactly         = $true
+                Times           = 3
+                Scope           = 'It'
+            }
+            Assert-MockCalled @assertMockCalledSplat
+        }
+
+        It "returns paginated results when fetching all spaces" {
+            Get-ConfluenceSpace
+            Get-ConfluenceSpace -Space "Foo", "Bar"
+
+            $assertMockCalledSplat = @{
+                CommandName     = "Invoke-Method"
+                ModuleName      = $env:BHProjectName
+                ParameterFilter = {
+                    $Uri -eq "/rest/api/space" -and
+                    $Paging -eq $true
+                }
+                Exactly         = $true
+                Times           = 1
+                Scope           = 'It'
+            }
+            Assert-MockCalled @assertMockCalledSplat
+
+            $assertMockCalledSplat["ParameterFilter"] = {
+                $Uri -like "/rest/api/space/*" -and
+                $Paging -eq $true
+            }
+            $assertMockCalledSplat["Times"] = 0
+            Assert-MockCalled @assertMockCalledSplat
+        }
+    }
+
+    Context "Parameter checking" {
+
+        #region Arrange
+        BeforeAll {
+            # backup current configuration
+            & (Get-Module AtlassianPS.Configuration) {
+                $script:previousConfig = $script:Configuration
+                $script:Configuration = @{}
+                $script:Configuration.Add("ConfluencePS", @{PageSize = 25})
+                $script:Configuration.Add("ServerList", [System.Collections.Generic.List[AtlassianPS.ServerData]]::new())
+            }
+            Add-AtlassianServerConfiguration -Name "lorem" -Uri "https://google.com" -Type CONFLUENCE -ErrorAction Stop
+        }
+        AfterAll {
+            #restore previous configuration
+            & (Get-Module AtlassianPS.Configuration) {
+                $script:Configuration = $script:previousConfig
+                Save-Configuration
+            }
+        }
+        $space = [AtlassianPS.ConfluencePS.Space]@{Key = "Foo"}
+        $invalidSpace = [AtlassianPS.ConfluencePS.Space]@{Id = 123}
+        #endregion Arrange
+
+        It "does not allow an empty ServerName" {
+            { Get-ConfluenceSpace -ServerName "" } | Should -Throw
+        }
+
+        It "does not allow a null ServerName" {
+            { Get-ConfluenceSpace -ServerName $null } | Should -Throw
+        }
+
+        It "completes ServerName arguments" {
+            $command = Get-Command -Name Get-ConfluenceSpace
+            $argumentCompleter = $command.Parameters["ServerName"].Attributes |
+                Where-Object {$_ -is [ArgumentCompleter]}
+            $completion = & $argumentCompleter.ScriptBlock
+
+            $completion.CompletionText | Should -Contain "lorem"
+        }
+
+        It "uses the `$PageSize when fetching results" {
+            Get-ConfluenceSpace
+            Get-ConfluenceSpace -PageSize 5
+
+            $assertMockCalledSplat = @{
+                CommandName     = "Invoke-Method"
+                ModuleName      = $env:BHProjectName
+                ParameterFilter = { $GetParameter["limit"] -eq 25 }
+                Exactly         = $true
+                Times           = 1
+                Scope           = 'It'
+            }
+            Assert-MockCalled @assertMockCalledSplat
+
+            $assertMockCalledSplat["ParameterFilter"] = { $GetParameter["limit"] -eq 5 }
+            Assert-MockCalled @assertMockCalledSplat
+        }
+
+        It "accepts a [String] as input for -Space" {
+            { Get-ConfluenceSpace -Space "Foo" } | Should -Not -Throw
+        }
+
+        It "accepts a [String] as input for -Space over the pipeline" {
+            { "Foo" | Get-ConfluenceSpace } | Should -Not -Throw
+        }
+
+        It "accepts a [AtlassianPS.ConfluencePS.Space] object as input for -Space" {
+            { Get-ConfluenceSpace -Space $space } | Should -Not -Throw
+        }
+
+        It "accepts a [AtlassianPS.ConfluencePS.Space] object as input for -Space over the pipeline" {
+            { $space | Get-ConfluenceSpace } | Should -Not -Throw
+        }
+
+        It "writes an error when an incomplete [AtlassianPS.ConfluencePS.Space] object is provided" {
+            { Get-ConfluenceSpace -Space $invalidSpace -ErrorAction Stop } | Should -Throw "Space is missing the Key"
+            { Get-ConfluenceSpace -Space $invalidSpace -ErrorAction SilentlyContinue } | Should -Not -Throw
+        }
+    }
+}
---
+++ docs/en-US/commands/Get-ConfluenceSpace.md
@@ -0,0 +1,217 @@
+---
+external help file: ConfluencePS-help.xml
+layout: documentation
+locale: en-US
+Module Name: ConfluencePS
+online version: https://atlassianps.org/docs/ConfluencePS/commands/Get-ConfluenceSpace/
+permalink: /docs/ConfluencePS/commands/Get-ConfluenceSpace/
+schema: 2.0.0
+---
+
+# Get-ConfluenceSpace
+
+## SYNOPSIS
+
+Retrieve a listing of spaces in your Confluence instance.
+
+## SYNTAX
+
+```powershell
+Get-ConfluenceSpace [-Space <Space[]>] [-PageSize <UInt32>] [-ServerName <String>]
+ [-Credential <PSCredential>] [-IncludeTotalCount] [-Skip <UInt64>] [-First <UInt64>]
+ [<CommonParameters>]
+```
+
+## DESCRIPTION
+
+Return all Confluence spaces, optionally filtering by Key.
+
+## EXAMPLES
+
+### EXAMPLE 1
+
+```powershell
+Get-ConfluenceSpace
+```
+
+Display the info of all spaces on the server.
+_Only spaces to which the current user has "read" permissions can be returned._
+
+### EXAMPLE 2
+
+```powershell
+Get-ConfluenceSpace -Space Hoth, Naboo | Format-List *
+```
+
+Return only the space with key "HOTH" and "NABOO" (case-insensitive).
+
+`Format-List *` displays all of the object's properties.
+
+### EXAMPLE 3
+
+```powershell
+Get-ConfluenceSpace -ServerName "myWiki" -Credential $cred
+```
+
+List all spaces found on the instance by manually specifying a server and
+authentication credentials. Provisioning of `-ServerName` and `-Credential` can
+be avoided by using `Connect-ConfluenceServer`.
+
+## PARAMETERS
+
+### -Space
+
+Identifies the Spaces to be looked up.
+
+> This parameter takes Space objects as input.
+> But a String can also be passed.
+> This will be used as "Key" for the space.
+
+```yaml
+Type: Space[]
+Parameter Sets: (All)
+Aliases: Key
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -PageSize
+
+Maximum number of results to fetch per call.
+
+This setting can be tuned to get better performance according to the load on the server.
+
+> Warning: too high of a PageSize can cause a timeout on the request.
+
+```yaml
+Type: UInt32
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: 25
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ServerName
+
+Name of the server registered in AtlassianPS.Configuration.
+
+This parameter supports tab-completion.
+
+> More information on how to authenticate in [about_ConfluencePS_Authentication](../../about/authentication.html)
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 0
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -Credential
+
+Confluence's credentials for authentication.
+
+> More information on how to authenticate in [about_ConfluencePS_Authentication](../../about/authentication.html)
+
+```yaml
+Type: PSCredential
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 1
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Skip
+
+Controls how many objects will be skipped before starting output.
+
+Defaults to 0.
+
+```yaml
+Type: UInt64
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: 0
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -First
+
+> NOTE: Not yet implemented.
+
+Indicates how many items to return.
+
+```yaml
+Type: UInt64
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: 18446744073709551615
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -IncludeTotalCount
+
+> NOTE: Not yet implemented.
+
+Causes an extra output of the total count at the beginning.
+
+Note this is actually a uInt64, but with a custom string representation.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+
+This cmdlet supports the common parameters: `-Debug`, `-ErrorAction`,
+`-ErrorVariable`, `-InformationAction`, `-InformationVariable`, `-OutVariable`,
+`-OutBuffer`, `-PipelineVariable`, `-Verbose`, `-WarningAction`, and
+`-WarningVariable`.
+For more information, see about_CommonParameters
+(<http://go.microsoft.com/fwlink/?LinkID=113216>).
+
+## INPUTS
+
+## OUTPUTS
+
+### ConfluencePS.Space
+
+## NOTES
+
+Piped output into other cmdlets is generally tested and supported.
+
+## RELATED LINKS
+
+[https://github.com/AtlassianPS/ConfluencePS](https://github.com/AtlassianPS/ConfluencePS)
